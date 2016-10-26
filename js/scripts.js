2ea4d68816b452b4896d1cdc2f39aa47e4a56bab
Lina Shadrach and taylor loftis-kim <lina@>
2016-10-26 14:51:42 -0700

    make backend functional and add front end code

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,57 @@
-var userInput = ("strength");
-var output = (userInput);
-var words = userInput.split(" ");
-var outPutArr = [];
-words.forEach(function(word){
-  word.split("");
-  checkWord(word);
-  //set checkword equal to var that can be pushed into output string
-})
+var pigLatin = function(userInput){
+  var words = userInput.split(" ");
+  var wordArr = [];
+  var outputArr = [];
+  var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "y", "Y"];
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n","p", "q", "r", "s", "t", "v", "w", "x", "z"];
 
-
-var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "y", "Y"];
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n","p", "q", "r", "s", "t", "v", "w", "x", "z"];
-var
-console.log(userInputArr);
-
-var checkWord = function(userInputArr)  {
-  userInputArr.push("-");
-  if(userInputArr.length > 1){
-    vowels.forEach(function(vowel)  {
-      if (userInputArr[0] === vowel && ) {
-        userInputArr.push("w");
-      }
-    });
-  }
-  var checkFirst = function() {
-    consonants.forEach(function(consonant){
-      if (userInputArr[0] === consonant) {
-        userInputArr.push(userInputArr.splice(0, 1));
-        checkFirst();
-      }
-      if (userInputArr[0] === ("q")) {
-        var quArr = userInputArr.splice(0,2);
-        var qu = quArr.join("");
-        userInputArr.push(qu);
-      }
-    });
-    return output;
-  };
-  checkFirst();
-  vowels.forEach(function(vowel)  {
-    if (userInputArr[0] === vowel)  {
-      userInputArr.push("ay");
+  var changeWord = function(word)  {
+    wordArr = word.slice();
+    wordArr.push("-");
+    if(wordArr.length > 2){
+      vowels.forEach(function(vowel)  {
+        if(wordArr[0] === vowel){
+          wordArr.push("w");
+        }
+      });
     }
+    var checkFirst = function(word) {
+      consonants.forEach(function(consonant){
+        if (wordArr[0] === consonant) {
+          wordArr.push(wordArr.splice(0, 1));
+          checkFirst(wordArr);
+        }
+        if (wordArr[0] === ("q")) {
+          var quArr = wordArr.splice(0,2);
+          var qu = quArr.join("");
+          wordArr.push(qu);
+        }
+      });
+      return output;
+    };
+    checkFirst(wordArr);
+    vowels.forEach(function(vowel)  {
+      if (wordArr[0] === vowel)  {
+        wordArr.push("ay");
+      }
+    });
+    return wordArr.join("");
+  };
+
+  words.forEach(function(word){
+    var letters = word.split("");
+    var pigLatinWord = changeWord(letters);
+    outputArr.push(pigLatinWord);
+    //set checkword equal to var that can be pushed into output string
   });
-  output = userInputArr.join("");
-
-}
-
-
-
-console.log(output);
-
+  var output = outputArr.join(" ");
+  return output;
+};
 
 $(document).ready(function()  {
-
+  $("form#sentence").submit(function(event){
+    var sentenceInput = $("input#textbox").val();
+    $(".output").text(pigLatin(sentenceInput));
+    event.preventDefault();
+  });
 });
